d4acadb2b0175046cada0084cede7242d8de345a
german <german.andaloro@gmail.com>
2015-06-05 14:34:55 -0300

    correccion user por code review

--- src/main/java/com/gm/tweak/domain/User.java
+++ src/main/java/com/gm/tweak/domain/User.java
@@ -2,25 +2,19 @@ package com.gm.tweak.domain;
 
 public class User {
 
-	private Long id;
+	private String id;
 	private String email;
 	private String username;
 	private String password;
 
-	// TODO CR-NANO: Tendria que tener el id por parametro, de lo contrario se
-	// puede cambiar el identificador al objeto y se violaria la integridad del
-	// objeto.
-	public User(String email, String username, String password) {
+	public User(String id, String email, String username, String password) {
 		this.email = email;
+		this.id = id;
 		this.username = username;
 		this.password = password;
 	}
 
-	public void assignId(long id) {
-		this.id = id;
-	}
-
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
 
@@ -36,6 +30,29 @@ public class User {
 		return password;
 	}
 
-	// TODO CR-NANO: Falta definir equals, es decir, el mecanismo por el cual un
-	// usuario es distinto a otro.
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
